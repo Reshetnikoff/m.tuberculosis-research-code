a0a713fb81290eb9e6414584686387f79cf59acc
reshetnikoff <kirill.reshetnikoff@gmail.com>
2022-11-10 17:05:01 +0300

    Add codes for testing aggregated features

---
+++ aggregated_features/lr_snp_indel_thr1.py
@@ -0,0 +1,167 @@
+# Evualation quality of prediction on logistic regression using Indels & SNPs 
+# without excluding rare mutations features datasets
+
+# Configuration part #
+
+# Directory with matrix without dataset split, without PFAM domain features 
+# and without excluding rare mutations
+bess_dir = "
+# Directory with information about data splits
+split_dir = f"./db/splits"
+# Output directory
+output_dir = f"./output/snp_indel"
+
+######################
+
+import os 
+import sys
+
+from multiprocessing import Pool
+
+import pandas as pd
+import numpy as np
+
+from scipy.sparse import csr_matrix
+from sklearn.metrics import roc_auc_score
+from sklearn.linear_model import LogisticRegression
+
+drug = sys.argv[1]
+
+output_dir = f"{output_dir}/{drug}"
+split_dir = f"{output_dir}/{drug}"
+
+N_ITERS = 100
+N_SPLIT = 5
+N_THREADS = 12
+
+def get_name_by_ind(pos_info, j, drug):
+    temp = pos_info.iloc[j]
+    gene, pos, ind1, ind2, act = temp['gene'], temp['pos'], temp['ind1'], temp['ind2'], temp['act']
+
+    if pos == 'changed':
+        return f"{gene}_domain"
+    elif pos == 'broken':
+        return f"{gene}_broken"
+    elif act == 'snp':
+        return f"{gene}_{pos}_{ind2}_snp"
+    elif ind2 == 'del':
+        return f"{gene}_{pos}_{ind1}_{ind2}"
+    elif act == 'del':
+        return f"{gene}_{pos}_{ind2}_{act}"
+    elif act == 'ins':
+        return f"{gene}_{pos}_{ind2}_{act}"
+
+def get_auc_dict(y_test, y_proba):
+    auc = 0
+    try:
+        auc = roc_auc_score(y_test, y_proba)
+    except ValueError:
+        pass
+    return auc
+
+def get_TN(y_test, y_pred):
+    TN = 0
+    for i in range(len(y_test)):
+        if y_test[i] == 0 and y_pred[i] == 0:
+            TN += 1
+    return TN
+
+def get_TP(y_test, y_pred):
+    TP = 0
+    for i in range(len(y_test)):
+        if y_test[i] == 1 and y_pred[i] == 1:
+            TP += 1
+    return TP
+
+def get_FP(y_test, y_pred):
+    FP = 0
+    for i in range(len(y_test)):
+        if y_test[i] == 0 and y_pred[i] == 1:
+            FP += 1
+    return FP
+
+def get_FN(y_test, y_pred):
+    FN = 0
+    for i in range(len(y_test)):
+        if y_test[i] == 1 and y_pred[i] == 0:
+            FN += 1
+    return FN
+
+if not os.path.exists(f"{output_dir}"):
+    os.makedirs(f"{output_dir}")
+
+data = pd.read_csv(f"{bess_dir}/{drug}.gt.1", sep=' ', 
+        index_col=None, header=None)
+pheno = pd.read_csv(f"{bess_dir}/{drug}.phen", sep=' ', index_col=None, header=None)
+pos_info = pd.read_csv(f"{bess_dir}/{drug}.gt.pos.1", sep='\t', 
+        index_col=None, header=None, usecols=[0, 1, 2, 3, 4])
+pos_info.columns = ['gene', 'pos', 'ind1', 'ind2', 'act']
+
+# Filtration
+data = data.transpose()
+
+temp = pos_info[(pos_info['act'] == 'ins') | (pos_info['act'] == 'snp') |
+        (pos_info['ind2'] == 'del') | (pos_info['act'] == 'del')]
+
+data = data[data.index.isin(temp.index)]
+data = data.transpose()
+
+y = pheno.to_numpy().ravel()
+
+
+result = pd.DataFrame(columns=['n_iter', 'n_split', 'auc', 'TN', 'TP', 'FP', 'FN'])
+k = 0
+
+for i in range(1, N_ITERS + 1):
+    for j in range(1, N_SPLIT + 1):
+        f = open(f"{split_dir}/{drug}.{i}.fold_{j}.train_indices")
+        train_index = [int(x[:-1]) if x[-1] == '\n' else int(x) for x in f.readlines()]
+        f.close()
+
+        test_index = [int(x) for x in data.index if int(x) not in train_index]
+
+        X_train = data.iloc[train_index, :]
+        X_test = data.iloc[test_index, :]
+        y_train = y[train_index]
+        y_test = y[test_index]
+  
+        X_train = X_train.transpose()
+        X_train.drop_duplicates(inplace=True)
+        X_train = X_train.transpose()
+
+        X_test = X_test[X_train.columns]
+
+        columns = X_train.columns
+
+        X_train = csr_matrix(X_train)
+        X_test = csr_matrix(X_test)
+
+        model = LogisticRegression(solver='saga', penalty='l1', max_iter=10000)
+        model.fit(X_train, y_train)
+
+        y_proba = model.predict_proba(X_test)[:, 1]
+        y_pred = model.predict(X_test) 
+        
+
+        coef = pd.DataFrame(data=model.coef_[0],
+                                columns=["coef"])
+
+        coef['ext_features'] = [get_name_by_ind(pos_info, int(x), drug) for x in columns]
+        coef.sort_values(by=['coef'], ascending=False, inplace=True)
+        coef.to_csv(f"{output_dir}/coef_{i}_{j}.csv",
+                          sep='\t', header=True, index=True)
+
+        auc = get_auc_dict(y_test, y_proba)
+        TP = get_TP(y_test, y_pred)
+        FP = get_FP(y_test, y_pred)
+        TN = get_TN(y_test, y_pred)
+        FN = get_FN(y_test, y_pred)
+
+        result.loc[k] = [i, j, auc, TN, TP, FP, FN]
+        k += 1
+    if i % 5 == 0:
+        result.to_csv(f"{output_dir}/result.csv", sep='\t', 
+                index=False, header=True)
+
+result.to_csv(f"{output_dir}/result.csv", sep='\t', 
+                index=False, header=True)
---
+++ aggregated_features/transform_domain.py
@@ -0,0 +1,118 @@
+# Script for converting PFAM domain features to different format
+
+# Configutation part #
+
+# Directory with phenotype data
+pheno_dir = "../db/pheno"
+# Directory with information about data splits
+split_dir = f"./db/splits"
+# Directory with domain data
+root_domain_dir = ""
+# Output directory
+output_dir = f"./domain_files_070222"
+# List of {gene}_{domain}
+pfam_features_file=  "../db/PfamFeatures.csv"
+
+######################
+
+import os
+import sys
+import time
+
+from multiprocessing import Pool
+
+import pandas as pd
+import numpy as np
+
+from scipy.sparse import save_npz, csc_matrix 
+
+start_time = time.time()
+
+drug = sys.argv[1]
+split_mode = sys.argv[2]
+
+output_dir = f"{output_dir}/{drug}"
+split_dir = f"{output_dir}/{drug}"
+
+if split_mode == "1":
+    splits = list(range(1, 17))
+elif split_mode == "2":
+    splits = list(range(17, 33))
+elif split_mode == "3":
+    splits = list(range(33, 49))
+elif split_mode == "4":
+    splits = list(range(49, 65))
+elif split_mode == "5":
+    splits = list(range(65, 81))
+elif split_mode == "6":
+    splits = list(range(81, 97))
+elif split_mode == "7":
+    splits = list(range(97, 101))
+elif split_mode == '8':
+    splits = [1, 2]
+
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+def transform_domain(split):
+    pheno = pd.read_csv(f"{pheno_dir}/{drug}.pheno", sep='\t', index_col=0, header=None)
+
+    if not os.path.exists(f"{output_dir}/{split}"):
+        os.mkdir(f"{output_dir}/{split}")
+    
+    for fold in range(1, 6):
+        start_fold_time = time.time()
+
+        f = open(f"{split_dir}/{drug}.{split}.fold_{fold}.train_indices")
+        train_indeces = [int(x[:-1]) if x[-1] == '\n' else int(x) for x in f.readlines()]
+        f.close()
+        test_indeces = [int(x) for x in range(len(pheno.index)) if x not in train_indeces]
+
+        domain_dir = f"{root_domain_dir}/{drug}/{split}/fold{fold}"
+
+        f = open(pfam_features_file)
+        features = [x[:-1] if x[-1] == '\n' else x for x in f.readlines()]
+        f.close()
+
+        domain_data = pd.DataFrame(index=features)
+        samples_dir = os.listdir(domain_dir)
+
+        for sample_name in pheno.index:
+            sample = f"{sample_name}_result.tsv"
+
+            f = open(f"{domain_dir}/{sample}")
+            temp = [x.split()[0] for x in f.readlines()  if x.split()[0] != '\n']
+            f.close()
+
+            domain_data[sample_name] = 0
+            domain_data.loc[temp,sample_name] = 1
+
+        domain_data = domain_data.loc[~(domain_data==0).all(axis=1)]
+        features = list(domain_data.index)
+
+        pos_domain_data = pd.DataFrame([[x, 'changed'] for x in features])
+        pos_domain_data.to_csv(f"{output_dir}/{split}/{drug}.gt.pos.domains.3.fold.{fold}", sep='\t', index=False, header=False)
+
+        domain_data = domain_data.transpose()        
+
+        output_data_train = csc_matrix(domain_data.iloc[train_indeces])
+        output_data_test = csc_matrix(domain_data.iloc[test_indeces])
+
+        save_npz(f"{output_dir}/{split}/{drug}.gt.domains.3.fold.{fold}.train.npz", output_data_train)
+        save_npz(f"{output_dir}/{split}/{drug}.gt.domains.3.fold.{fold}.test.npz", output_data_test)
+
+        pheno.iloc[train_indeces].to_csv(f"{output_dir}/{split}/{drug}.pheno.fold.{fold}.train",
+                        sep=' ', index=False, header=False)
+        pheno.iloc[test_indeces].to_csv(f"{output_dir}/{split}/{drug}.pheno.fold.{fold}.test",
+                        sep=' ', index=False, header=False)
+
+        end_fold_time = time.time()
+        print(f"Fold {fold} done for {drug}: {end_fold_time - start_fold_time}")
+
+
+p = Pool(len(splits))
+p.map(transform_domain, splits)
+
+end_time = time.time()
+print(end_time - start_time)
